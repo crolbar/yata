f54b486e1dcc1b85ccc11c5583baa43217218015
crolbar <crolbar@crolbar.com>
2024-11-13 18:44:58 +0200

    fix: prevent sql injections

--- src/models/TaskModel.php
+++ src/models/TaskModel.php
@@ -19,25 +19,35 @@ class TaskModel
 
     public static function deleteById(int $id): void
     {
-        $pdo = Database::getConnection();
+        $pdo    = Database::getConnection();
+        $query  = "delete from tasks where id = :id";
+        $stmt   = $pdo->prepare($query);
 
-        $query = "delete from tasks where id = $id";
-        $pdo->exec($query);
+        $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+
+        $stmt->execute();
     }
 
     public static function createTask(string $title): void
     {
-        $pdo = Database::getConnection();
+        $pdo    = Database::getConnection();
+        $query  = "insert into tasks (title) values (:title)";
+        $stmt   = $pdo->prepare($query);
 
-        $query = "insert into tasks (title) values ('$title')";
-        $pdo->exec($query);
+        $stmt->bindValue(":title", $title, PDO::PARAM_STR);
+
+        $stmt->execute();
     }
 
     public static function updateTask(string $id, string $title): void
     {
-        $pdo = Database::getConnection();
+        $pdo    = Database::getConnection();
+        $query  = "update tasks set title = :title where id = :id";
+        $stmt   = $pdo->prepare($query);
 
-        $query = "update tasks set title = '$title' where id = $id";
-        $pdo->exec($query);
+        $stmt->bindValue(":title",  $title, PDO::PARAM_STR);
+        $stmt->bindValue(":id",     $id,    PDO::PARAM_INT);
+
+        $stmt->execute();
     }
 }
