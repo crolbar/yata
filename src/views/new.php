b1ff0aba972b7dba94c8e9f046b65b64333fc771
crolbar <crolbar@crolbar.com>
2024-11-21 17:52:30 +0200

    ui: add & refresh + throbbler buttons

--- src/views/new.php
+++ src/views/new.php
@@ -120,7 +120,16 @@ function renderGridHeader(DateTime $week_start): string
     return <<<HTML
         <div id="grid-header-container">
             <div class="grid grid-cols-8 gap-1 bg-neutral-800 sticky top-0 z-10">
-                <div class="p-2 bg-neutral-800"></div>
+                <div class="flex relative justify-between bg-neutral-800">
+                    <button id="task-add" class="border border-neutral-700 rounded opacity-50 hover:opacity-100 hover:bg-neutral-700 bg-neutral-900 w-full h-full">
+                        <svg class="inline w-5 h-5 bg-transparent fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z"/></svg>
+                    </button>
+
+                    <button id="tasks-refresh" class="border border-neutral-700 rounded opacity-50 hover:opacity-100 hover:bg-neutral-700 bg-neutral-900 w-full h-full">
+                        <svg id="tasks-refresh-svg" class="inline w-5 h-5 bg-transparent fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M105.1 202.6c7.7-21.8 20.2-42.3 37.8-59.8c62.5-62.5 163.8-62.5 226.3 0L386.3 160 352 160c-17.7 0-32 14.3-32 32s14.3 32 32 32l111.5 0c0 0 0 0 0 0l.4 0c17.7 0 32-14.3 32-32l0-112c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 35.2L414.4 97.6c-87.5-87.5-229.3-87.5-316.8 0C73.2 122 55.6 150.7 44.8 181.4c-5.9 16.7 2.9 34.9 19.5 40.8s34.9-2.9 40.8-19.5zM39 289.3c-5 1.5-9.8 4.2-13.7 8.2c-4 4-6.7 8.8-8.1 14c-.3 1.2-.6 2.5-.8 3.8c-.3 1.7-.4 3.4-.4 5.1L16 432c0 17.7 14.3 32 32 32s32-14.3 32-32l0-35.1 17.6 17.5c0 0 0 0 0 0c87.5 87.4 229.3 87.4 316.7 0c24.4-24.4 42.1-53.1 52.9-83.8c5.9-16.7-2.9-34.9-19.5-40.8s-34.9 2.9-40.8 19.5c-7.7 21.8-20.2 42.3-37.8 59.8c-62.5 62.5-163.8 62.5-226.3 0l-.1-.1L125.6 352l34.4 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L48.4 288c-1.6 0-3.2 .1-4.8 .3s-3.1 .5-4.6 1z"/></svg>
+                    </button>
+                </div>
+
                 $header
             </div>
         </div>
@@ -291,11 +300,6 @@ $week_start = clone $date->modify('monday this week');
     </head>
     <body>
         <div class="container mx-auto px-4 py-8 h-screen">
-
-            <button id="refresh">refresh</button>
-            <br/>
-            <button id="add">add</button>
-
             <div id="navigation-container">
                 <?= renderNavigationControls($week_start) ?>
             </div>
@@ -400,6 +404,8 @@ $week_start = clone $date->modify('monday this week');
             }
 
             const sendRequest = (path, method, data) => {
+                toggleThrobbler();
+
                 fetch(path, {
                     method: method,
                     body: data,
@@ -445,7 +451,6 @@ $week_start = clone $date->modify('monday this week');
             function loadTasks(resp) {
                 document.querySelectorAll('#task-block').forEach(task => task.remove());
 
-                // TODO
                 for (task of resp) {
                     const start = parseInt(task.start_time);
                     const end = parseInt(task.end_time);
@@ -457,19 +462,23 @@ $week_start = clone $date->modify('monday this week');
                 }
 
                 initTaskBlockListeners()
+                toggleThrobbler();
+            }
+
+            function toggleThrobbler() {
+                const svg = document.getElementById('tasks-refresh-svg');
+                svg.classList.toggle('animate-spin')
             }
 
             function fetchTasks() {
                 sendRequest('ajax/task/fetchall', 'GET', null)
             }
 
-            // tmp
-            document.getElementById('refresh').addEventListener("click", () => {
+            document.getElementById('tasks-refresh').addEventListener("click", () => {
                 fetchTasks();
             })
 
-            // tmp
-            document.getElementById('add').addEventListener("click", () => {
+            document.getElementById('task-add').addEventListener("click", () => {
                 document.querySelector('#task-form').reset()
                 toggleTaskDialog();
             })
