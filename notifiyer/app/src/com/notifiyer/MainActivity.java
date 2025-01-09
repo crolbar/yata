27386c6dae7b8552c107829bd1a0e4ff14138ea0
crolbar <crolbar@crolbar.com>
2025-01-09 21:10:56 +0200

    notif: some ui

--- notifiyer/app/src/com/notifiyer/MainActivity.java
+++ notifiyer/app/src/com/notifiyer/MainActivity.java
@@ -1,21 +1,49 @@
 package com.notifiyer;
 
 import android.content.Intent;
-import android.util.Log;
 import android.os.Bundle;
-import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+
 import androidx.core.app.ActivityCompat;
+
+import com.bumptech.glide.Glide;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
 
 public
 class MainActivity extends AppCompatActivity
 {
+  private
     Auth auth;
+
+  public
+    String username = "";
+  public
+    String profileImageUri = "";
+
+  public
+    String fcm_token = "";
+  public
+    String sub = "";
+
+  private
+    ImageView profileImageView;
+  private
+    TextView usernameTextView;
+  private
+    Button signInOutButton;
+  private
+    Switch rcNotifSwitch;
 
     @Override protected void onCreate(Bundle savedInstanceState)
     {
@@ -26,21 +54,53 @@ class MainActivity extends AppCompatActivity
 
         this.auth = new Auth(this);
 
+        this.profileImageView = findViewById(R.id.profile_image_view);
+        this.usernameTextView = findViewById(R.id.username_text_view);
+        this.signInOutButton = findViewById(R.id.btn_sign_in_out);
+        this.rcNotifSwitch = findViewById(R.id.rc_notif_switch);
 
+        updateUI();
 
-        findViewById(R.id.my_button).setOnClickListener(view->{
-            new Handler().postDelayed(
-              ()->Notify.showNotification(
-                "title for notif", "msg for notif", this),
-              2000);
-            Log.w("button", "button click");
+        this.rcNotifSwitch.setOnCheckedChangeListener((buttonView, isChecked)->{
+            if (isChecked) {
+                Log.d("Switch", "Switch is ON");
+            } else {
+                Log.d("Switch", "Switch is OFF");
+            }
         });
 
-        findViewById(R.id.btn_google_sign_in)
-          .setOnClickListener(v->this.auth.signIn());
+        this.signInOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v)
+            {
+                auth.signInOut();
+            }
+        });
+    }
 
-        findViewById(R.id.btn_google_sign_out)
-          .setOnClickListener(v->this.auth.signOut());
+  public
+    void updateUI()
+    {
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            profileImageView.setVisibility(View.GONE);
+            rcNotifSwitch.setVisibility(View.GONE);
+            usernameTextView.setVisibility(View.GONE);
+
+            usernameTextView.setText("");
+            signInOutButton.setText("Sign In");
+
+            return;
+        }
+
+        this.auth.updateInfo(()->{
+            profileImageView.setVisibility(View.VISIBLE);
+            rcNotifSwitch.setVisibility(View.VISIBLE);
+            usernameTextView.setVisibility(View.VISIBLE);
+
+            Glide.with(this).load(profileImageUri).into(profileImageView);
+
+            usernameTextView.setText(this.username);
+            signInOutButton.setText("Sign Out");
+        });
     }
 
     @Override protected void onStart()
