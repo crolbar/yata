44818a46e09956b285897421025fc764f56dc4d0
crolbar <crolbar@crolbar.com>
2025-04-27 20:41:34 +0300

    flake: fix android home var

--- flake.nix
+++ flake.nix
@@ -16,9 +16,22 @@
 
         inherit (pkgs) lib;
         scripts = import ./scripts.nix {inherit pkgs lib;};
-        notifiyer-pkgs = import ./notifiyer/pkgs.nix {inherit pkgs;};
+
+        sdkVersion = "34";
+        buildToolsVersion = "34.0.0";
+        # platformToolsVersion = "34.0.1";
+
+        androidComposition = pkgs.androidenv.composeAndroidPackages {
+          includeNDK = false;
+          includeSystemImages = false;
+          includeEmulator = false;
+          platformVersions = [sdkVersion];
+          buildToolsVersions = [buildToolsVersion];
+          # platformToolsVersion = platformToolsVersion;
+        };
+        androidSdk = androidComposition.androidsdk;
       in {
-        default = pkgs.mkShell {
+        default = pkgs.mkShell rec {
           packages = with pkgs;
             [
               phpactor # lsp
@@ -28,13 +41,39 @@
 
               docker-compose
               flyctl # deployment
+
+              #android-studio
+              gradle
+
+              jdk
+
+              androidComposition.build-tools
+
+              (pkgs.writers.writeBashBin "prun" ''
+                # 192.168.1.3:42531
+                # adb connect 192.168.1.3:42531
+                # adb -s $dev uninstall com.notifiyer
+
+                dev=""
+
+                if [[ "$1" == "e" ]]; then
+                    dev="emulator-5554"
+                else
+                    dev="192.168.1.2:40819"
+                fi
+
+                adb -s $dev install app/build/outputs/apk/debug/app-debug.apk
+                adb -s $dev shell am start -n com.notifiyer/.MainActivity
+              '')
             ]
-            ++ scripts
-            ++ notifiyer-pkgs;
+            ++ scripts;
 
           buildInputs = with pkgs; [
             php83
           ];
+
+          ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+          GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${ANDROID_SDK_ROOT}/build-tools/${buildToolsVersion}/aapt2";
         };
       }
     );
